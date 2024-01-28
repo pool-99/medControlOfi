6c20ca5e6e8e033d2a694b602562e41b8006c9bc
David-Sac <saccsaradavid@gmail.com>
2024-01-28 10:46:42 -0500

    Reloj

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+import 'dart:async';
 
 void main() {
-  runApp(MedicamentosApp());
+  runApp(const MedicamentosApp());
 }
 
 class MedicamentosApp extends StatelessWidget {
+  const MedicamentosApp({Key? key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,6 +18,8 @@ class MedicamentosApp extends StatelessWidget {
 }
 
 class Medicamentos extends StatefulWidget {
+  const Medicamentos({Key? key});
+
   @override
   _MedicamentosState createState() => _MedicamentosState();
 }
@@ -35,15 +40,15 @@ class _MedicamentosState extends State<Medicamentos> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromARGB(209, 32, 197, 120),
-        title: Text('Medicamentos'),
+        title: const Text('Medicamentos'),
         leading: IconButton(
-          icon: Icon(Icons.person),
+          icon: const Icon(Icons.person),
           onPressed: () {
             print('Icono de usuario presionado');
           },
         ),
       ),
-      body: _pages[_currentIndex],
+      body: _currentIndex == 0 ? HoyPage() : _pages[_currentIndex],
       bottomNavigationBar: ConvexAppBar(
         backgroundColor: Color.fromARGB(209, 32, 197, 120),
         items: [
@@ -72,6 +77,64 @@ class PlaceholderWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Text(text),
+    );
+  }
+}
+
+class HoyPage extends StatefulWidget {
+  @override
+  _HoyPageState createState() => _HoyPageState();
+}
+
+class _HoyPageState extends State<HoyPage> {
+  late Timer _timer;
+  int _duration = 24 * 60 * 60; // Duración de 24 horas en segundos
+
+  @override
+  void initState() {
+    super.initState();
+    _startTimer();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  void _startTimer() {
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if (_duration > 0) {
+          _duration--;
+        } else {
+          _timer.cancel();
+        }
+      });
+    });
+  }
+
+  String _formatDuration(int seconds) {
+    Duration duration = Duration(seconds: seconds);
+    return '${duration.inHours}:${(duration.inMinutes % 60).toString().padLeft(2, '0')}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Tiempo Restante'),
+          Text(
+            _formatDuration(_duration),
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
