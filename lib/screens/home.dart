671b23680328bad4d77fa465615353b844482d3d
pool-99 <poolpina08@gmail.com>
2024-01-29 00:18:44 -0500

    avance

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
-import 'dart:async';
 
 void main() {
-  runApp(const MedicamentosApp());
+  runApp(MedicamentosApp());
 }
 
 class MedicamentosApp extends StatelessWidget {
-  const MedicamentosApp({Key? key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,8 +15,6 @@ class MedicamentosApp extends StatelessWidget {
 }
 
 class Medicamentos extends StatefulWidget {
-  const Medicamentos({Key? key});
-
   @override
   _MedicamentosState createState() => _MedicamentosState();
 }
@@ -40,15 +35,15 @@ class _MedicamentosState extends State<Medicamentos> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromARGB(209, 32, 197, 120),
-        title: const Text('Medicamentos'),
+        title: Text('Medicamentos'),
         leading: IconButton(
-          icon: const Icon(Icons.person),
+          icon: Icon(Icons.person),
           onPressed: () {
             print('Icono de usuario presionado');
           },
         ),
       ),
-      body: _currentIndex == 0 ? HoyPage() : _pages[_currentIndex],
+      body: _pages[_currentIndex],
       bottomNavigationBar: ConvexAppBar(
         backgroundColor: Color.fromARGB(209, 32, 197, 120),
         items: [
@@ -81,55 +76,13 @@ class PlaceholderWidget extends StatelessWidget {
   }
 }
 
-<<<<<<< HEAD
 class MedicamentosPage extends StatelessWidget {
-=======
-class HoyPage extends StatefulWidget {
-  @override
-  _HoyPageState createState() => _HoyPageState();
-}
-
-class _HoyPageState extends State<HoyPage> {
-  late Timer _timer;
-  int _duration = 24 * 60 * 60; // Duración de 24 horas en segundos
-
-  @override
-  void initState() {
-    super.initState();
-    _startTimer();
-  }
-
-  @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
-  }
-
-  void _startTimer() {
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      setState(() {
-        if (_duration > 0) {
-          _duration--;
-        } else {
-          _timer.cancel();
-        }
-      });
-    });
-  }
-
-  String _formatDuration(int seconds) {
-    Duration duration = Duration(seconds: seconds);
-    return '${duration.inHours}:${(duration.inMinutes % 60).toString().padLeft(2, '0')}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}';
-  }
-
->>>>>>> 6c20ca5e6e8e033d2a694b602562e41b8006c9bc
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-<<<<<<< HEAD
           // Contenido de la página de Medicamentos
           Text('Contenido de Medicamentos'),
 
@@ -148,14 +101,6 @@ class _HoyPageState extends State<HoyPage> {
             label: Text('Agregar medicamentos'),
             style: ElevatedButton.styleFrom(
               backgroundColor: Color.fromARGB(209, 32, 197, 120), // Color del botón
-=======
-          Text('Tiempo Restante'),
-          Text(
-            _formatDuration(_duration),
-            style: TextStyle(
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold,
->>>>>>> 6c20ca5e6e8e033d2a694b602562e41b8006c9bc
             ),
           ),
         ],
@@ -163,7 +108,6 @@ class _HoyPageState extends State<HoyPage> {
     );
   }
 }
-<<<<<<< HEAD
 
 class OtraPagina extends StatelessWidget {
   @override
@@ -178,5 +122,3 @@ class OtraPagina extends StatelessWidget {
     );
   }
 }
-=======
->>>>>>> 6c20ca5e6e8e033d2a694b602562e41b8006c9bc
