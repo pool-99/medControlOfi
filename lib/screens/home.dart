e53e838d6871127714fec5659d4e86de7d556fa6
pool-99 <poolpina08@gmail.com>
2024-01-30 13:19:20 -0500

    consejo y hoy

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:met_control/pages/agregarMedicina.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
 
 void main() {
   runApp(MedicamentosApp());
@@ -25,10 +26,10 @@ class _MedicamentosState extends State<Medicamentos> {
 
   // Lista de páginas correspondientes a cada ícono del ConvexAppBar
   final List<Widget> _pages = [
-    PlaceholderWidget('Hoy Page'),
+    HoyPage(),
     PlaceholderWidget('Mi Historia Page'),
     MedicamentosPage(),
-    PlaceholderWidget('Consejos Page'),
+    ConsejosPage(),
   ];
 
   @override
@@ -110,3 +111,200 @@ class MedicamentosPage extends StatelessWidget {
   }
 }
 
+
+class HoyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Contenido de la página de Hoy
+
+          // Primer Contenedor Reloj
+          Container(
+            width: 150,
+            height: 150,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 3,
+                  blurRadius: 5,
+                  offset: Offset(0, 3), // Cambia la posición de la sombra
+                ),
+              ],
+            ),
+              child: CircularStepProgressIndicator(
+                totalSteps: 100,
+                currentStep: 74,
+                stepSize: 10,
+                selectedColor: Colors.greenAccent,
+                unselectedColor: Colors.grey[200],
+                padding: 0,
+                width: 150,
+                height: 150,
+                selectedStepSize: 15,
+                roundedCap: (_, __) => true,
+              ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ConsejosPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: MyGrid(),
+      ),
+    );
+  }
+}
+
+class MyGrid extends StatelessWidget {
+  final List<String> imageConsejos = [
+    'assets/img/Consejo01.png',
+    'assets/img/Consejo02.png',
+    'assets/img/Consejo03.png',
+    'assets/img/Consejo04.png',
+    'assets/img/Consejo05.png',
+    'assets/img/Consejo06.png',
+    'assets/img/Consejo07.png',
+    'assets/img/Consejo08.png',
+    'assets/img/Consejo09.png',
+  ];
+
+  final List<String> titles = [
+    'Comer Saludable',
+    'Importancia de vacunarse',
+    'No Alcohol',
+    'No Fumar',
+    'Camina',
+    'Bebes',
+    'Chequeos',
+    'Cuerpo Sano',
+    'Estres',
+  ];
+
+  final List<String> descriptions = [
+    'Come alimentos saludables para mantener un estilo de vida equilibrado.',
+    'La vacunación es crucial para prevenir enfermedades y proteger a la comunidad.',
+    'Evita el consumo de alcohol para cuidar tu salud.',
+    'Deja de fumar para mejorar tu salud pulmonar y cardiovascular.',
+    'Realiza caminatas diarias para mantener un cuerpo activo y saludable.',
+    'Cuida a los más pequeños y asegúrate de satisfacer sus necesidades.',
+    'Realiza chequeos médicos periódicos para detectar posibles problemas de salud.',
+    'Adopta hábitos que promuevan un cuerpo sano y fuerte.',
+    'Gestiona el estrés de manera efectiva para mejorar tu bienestar emocional.',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 8.0,
+      ),
+      itemCount: imageConsejos.length,
+      itemBuilder: (BuildContext context, int index) {
+        return GestureDetector(
+          onTap: () {
+            // Mostrar el widget de detalle cuando se toque una imagen
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return DetalleConsejo(
+                  imageConsejo: imageConsejos[index],
+                  title: titles[index],
+                  description: descriptions[index],
+                  onClose: () {
+                    Navigator.of(context).pop(); // Cerrar el cuadro de diálogo
+                  },
+                );
+              },
+            );
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8.0),
+              color: Color.fromARGB(255, 164, 197, 207),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                
+                ClipOval(
+                child: Image.network(
+                  imageConsejos[index],
+                  width: 100,
+                  height: 100,
+                  fit: BoxFit.cover,
+                  
+                ),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  titles[index],
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class DetalleConsejo extends StatelessWidget {
+  final String imageConsejo;
+  final String title;
+  final VoidCallback onClose;
+  final String description;
+
+
+  DetalleConsejo({required this.imageConsejo, required this.title, required this.description, required this.onClose});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      content: Column(
+        children: [
+          Image.network(
+            imageConsejo,
+            width: 150,
+            height: 150,
+            fit: BoxFit.cover,
+          ),
+          SizedBox(height: 8.0),
+          
+          Text(
+            title,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            description,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 16.0),
+          ),
+          SizedBox(height: 8.0),
+          SizedBox(height: 30.0),
+          ElevatedButton(
+            onPressed: onClose,
+            child: Text('Volver a Consejos'),
+          ),
+        ],
+      ),
+    );
+  }
+}
